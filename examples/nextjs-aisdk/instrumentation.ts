570b602f4cf1c88c8b7f060e3160f627cd5b6c68
Dinmukhamed Mailibay <47117969+dinmukhamedm@users.noreply.github.com>
2025-07-01 23:37:00 +0100

    update deps in examples to fix dependabot (#97)
    * update deps in examples to fix dependabot
    
    * also comment out registerOtel

--- examples/nextjs-aisdk/instrumentation.ts
+++ examples/nextjs-aisdk/instrumentation.ts
@@ -1,11 +1,14 @@
 import { registerOTel } from '@vercel/otel';
 
 export async function register() {
-  // Only use this function if you want to send Next.js traces to DataDog or New Relic.
+  // Only use this function if you want to send Next.js traces to an
+  // observability platform.
   // Otherwise, you can just use the `Laminar.initialize()` directly.
-  registerOTel({
-    serviceName: "therapy-service",
-  });
+
+  // registerOTel({
+  //   serviceName: "therapy-service",
+  // });
+
   if (process.env.NEXT_RUNTIME === "nodejs") {
     const { Laminar } = await import("@lmnr-ai/lmnr");
 
