ff703dd53a66267d79ea36b8e2aaf89eb5b6d96b
Din <dinmukhamed.mailibay@gmail.com>
2025-08-11 14:57:46 +0100

    fix snake_case contexts

--- src/laminar.ts
+++ src/laminar.ts
@@ -486,15 +486,15 @@ export class Laminar {
     let parentIdsPath: string[] | undefined;
 
     if (parentSpanContext) {
-      if (typeof parentSpanContext !== 'string' && 'path' in parentSpanContext) {
+      if (typeof parentSpanContext !== 'string') {
         parentPath = parentSpanContext.spanPath;
         parentIdsPath = parentSpanContext.spanIdsPath;
       }
       if (typeof parentSpanContext === 'string') {
         try {
           const parsed = JSON.parse(parentSpanContext);
-          parentPath = parsed.spanPath;
-          parentIdsPath = parsed.spanIdsPath;
+          parentPath = parsed.spanPath ?? parsed.span_path;
+          parentIdsPath = parsed.spanIdsPath ?? parsed.span_ids_path;
         } catch (e) {
           logger.warn("Failed to parse parent span context: " +
             (e instanceof Error ? e.message : String(e)),
